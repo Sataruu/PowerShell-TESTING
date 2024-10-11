1feec32c75ffe7093d1c9ebe17d4cfe25988cc0f
Sataruu <143044183+Sataruu@users.noreply.github.com>
2024-10-11 12:31:40 +0100

    Add files via upload

---
+++ practice2.ps1
@@ -0,0 +1,11 @@
+﻿#practice2
+
+#hashtable1
+$daysworked=@{John=12; Joe=20; Mary=18}
+
+#hashtable2
+$SalaryPerDay = @{John=100; Joe="120";Mary="150"}
+
+
+#hashtable3
+$Salary = @{ John = $daysworked.John * $SalaryPerDay.John ; Joe = $daysworked.Joe * $SalaryPerDay.Joe ; Mary = $daysworked.Mary * $SalaryPerDay.Mary;}
